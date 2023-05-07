62a3895aafff3d46e3fb283053d9872cdce029a4
azernn <azerinomarli@gmail.com>
2023-05-07 06:58:22 -0700

    Contact part is created

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import { Header } from "./app/components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./app/pages/HomePage/HomePage";
+import { Contact } from "./app/pages/HomePage/components/Contact/Contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element = {<HomePage />}></Route>
         </Routes>
+        <Contact/>
       </main>
     </>
   );
