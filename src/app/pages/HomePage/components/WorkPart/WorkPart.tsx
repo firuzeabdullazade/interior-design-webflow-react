930180741b596826bd4c406f11630f2247696ab0
heraden <kerimovaleyla.041@gmail.com>
2023-05-09 17:10:27 +0400

    Headers of Work Part were changed

--- src/app/pages/HomePage/components/WorkPart/WorkPart.tsx
+++ src/app/pages/HomePage/components/WorkPart/WorkPart.tsx
@@ -5,28 +5,30 @@ export const WorkPart = () => {
         <>
             <div className="work-part">
                 <div className="info">
-                    <div className="head">Project Plan</div>
-                    <div className="subhead">
+                    <h2>Project Plan</h2>
+                    <h3>
                         Efficient execution of our interior design project plan will ensure timely completion while delivering exceptional results that exceed our clients' expectations
-                    </div>
+                    </h3>
                     <button>
                         <div className="text">Read More</div>
                         <img src={arrow} alt="" />
                     </button>
                 </div>
-                <div className="info"><div className="head">Interior Work</div>
-                    <div className="subhead">
+                <div className="info">
+                    <h2>Interior Work</h2>
+                    <h3>
                     Expertly executing the interior work plan is essential to bringing your design vision to life, creating a harmonious and inviting space that reflects your unique style and personality.
-                    </div>
+                    </h3>
                     <button>
                         <div className="text">Read More</div>
                         <img src={arrow} alt="" />
                     </button>
                 </div>
-                <div className="info"><div className="head">Realization</div>
-                    <div className="subhead">
+                <div className="info">
+                    <h2>Realization</h2>
+                    <h3>
                     Closely adhering to our project realization plan will ensure a successful interior design outcome that fulfills both functional requirements and aesthetic preferences.
-                    </div>
+                    </h3>
                     <button>
                         <div className="text">Read More</div>
                         <img src={arrow} alt="" />
