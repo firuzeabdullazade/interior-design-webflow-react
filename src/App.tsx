7a6592a05e9c34bdafc44df8a9e5e7b37fbd985c
shukufabayramzada <shukufabayramzada@gmail.com>
2023-05-11 17:30:51 +0400

    Login Page was created

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from "./app/components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 import { Footer } from "./app/components/Footer/Footer";
 import { HomePage } from "./app/pages/HomePage/HomePage";
+import { LoginPage } from "./app/pages/LoginPage/LoginPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element = {<HomePage />}></Route>
+          <Route path="login" element={<LoginPage />}></Route>
         </Routes>
       </main>
       <Footer/>
--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,7 +2,17 @@ import "./Header.scss";
 import pageLogo from "../../../assets/icons/pageLogo.svg";
 import searchIcon from "../../../assets/icons/search icon.svg";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../store/store";
+import { logOut } from "../../shared/UserWidget/userWidget.slice";
+import { UserWidget } from "../../shared/UserWidget/UserWidget";
 export const Header = () => {
+  const userWidgetState = useSelector((state: RootState) => state.userWidget);
+  const dispatch = useDispatch();
+
+  const onLogOutClick = () => {
+    dispatch(logOut());
+  };
   return (
     <header>
       <div className="header-container">
@@ -15,7 +25,17 @@ export const Header = () => {
             <Link to="" className="nav-link">Project</Link>
             <Link to="" className="nav-link">Blog</Link>
             <Link to="" className="nav-link">Contact</Link>
-            <Link to="" className="nav-link">Login</Link>
+            {
+            !userWidgetState.name && <>
+              <Link to="/login" className="nav-link">Login</Link>
+            </>
+          }
+          {
+            userWidgetState.name && <>
+              <button onClick={onLogOutClick}>Log out</button>
+              <UserWidget />
+            </>
+          }
           </nav>
           <img src={searchIcon} alt="" />
         </div>
