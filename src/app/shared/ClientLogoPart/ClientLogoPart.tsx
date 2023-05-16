602b09c85c7607c9c29015444f4fd56f1eb37e84
firuzeabdullazade <firuzeabdullazade64@gmail.com>
2023-05-16 14:48:38 +0400

    max-width was added to body, Work part was changed

--- src/app/shared/ClientLogoPart/ClientLogoPart.tsx
+++ src/app/shared/ClientLogoPart/ClientLogoPart.tsx
@@ -1,9 +1,9 @@
 import './ClientLogoPart.scss';
 import firstLogo from '../../../assets/icons/01.svg';
-import secondLogo from '../../../assets/icons/02.svg'
-import thirdLogo from '../../../assets/icons/03 (1).svg'
-import fourthLogo from '../../../assets/icons/04.svg'
-import fifthLogo from '../../../assets/icons/05 - Copy (2).svg'
+import secondLogo from '../../../assets/icons/02.svg';
+import thirdLogo from '../../../assets/icons/03 (1).svg';
+import fourthLogo from '../../../assets/icons/04.svg';
+import fifthLogo from '../../../assets/icons/05 - Copy (2).svg';
 export const ClientLogoPart = () => {
     return (
         <div className="logo-part">
@@ -13,5 +13,5 @@ export const ClientLogoPart = () => {
             <img src={fourthLogo} alt="" />
             <img src={fifthLogo} alt="" />
         </div>
-    )
+    );
 };
