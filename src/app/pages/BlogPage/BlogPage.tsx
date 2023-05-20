2106f2bd7c414b6e7b7d0db3c666eb6dec505370
firuzeabdullazade <firuzeabdullazade64@gmail.com>
2023-05-20 22:28:26 +0400

    Latest Post was added

--- src/app/pages/BlogPage/BlogPage.tsx
+++ src/app/pages/BlogPage/BlogPage.tsx
@@ -3,12 +3,31 @@ import './BlogPage.scss';
 import { RootState } from '../../../store/store';
 import { ArticlesGridItem } from './components/ArticlesGridItem/ArticlesGridItem';
 import { Banner } from './components/Banner/Banner';
+import { LatestPost } from './components/LatestPost/LatestPost';
 
 export const BlogPage = () => {
   const blogPageState = useSelector((state: RootState) => state.blogPage);
+
   return (
     <>
       <Banner />
+      <div className="latest-post">
+        <h2>Latest Post</h2>
+        {blogPageState.posts.map((post) => (
+          <LatestPost
+            key={`post-${post.imageSrc}`}
+            imageSrc={post.imageSrc}
+            tag={post.tag}
+            date={post.date}
+            content={post.content}
+            postId={post.postId}
+            firstPostImage={post.firstPostImage}
+            secondPostImage={post.secondPostImage}
+            postContent={post.postContent}
+            text={post.text}
+          />
+        ))}
+      </div>
       <section className="blog-page">
         <div className="blog-part">
           <div className="heading">
--- src/app/pages/BlogPage/blogPage.slice.ts
+++ src/app/pages/BlogPage/blogPage.slice.ts
@@ -10,6 +10,7 @@ import secondArticle from '../../../assets/images/secondArticle.png';
 import secondKitchanImage from '../../../assets/images/secondKitchanImage.png';
 import sixthArticle from '../../../assets/images/sixthArticle.png';
 import thirdArticle from '../../../assets/images/thirdArticle.png';
+import firstPostPicture from '../../../assets/images/firstPostPicture.png';
 import { createSlice } from '@reduxjs/toolkit';
 
 interface ArticlesGridItem {
@@ -23,8 +24,21 @@ interface ArticlesGridItem {
   postContent: string;
 }
 
+interface PostsProps {
+  postId: number;
+  date: string;
+  tag: string;
+  imageSrc: string;
+  content: string;
+  firstPostImage: string;
+  secondPostImage: string;
+  postContent: string;
+  text: string;
+}
+
 interface BlogPageState {
   articles: ArticlesGridItem[];
+  posts: PostsProps[];
 }
 
 const initialState: BlogPageState = {
@@ -37,8 +51,7 @@ const initialState: BlogPageState = {
       firstPostImage: firstKitchanImage,
       secondPostImage: secondKitchanImage,
       content: 'Let’s Get Solution For Building Construction Work',
-      postContent: 
-      `<p>
+      postContent: `<p>
       The kitchen is often referred to as the heart of the home, where culinary delights are created, memories are made, and families gather. Designing a functional and visually appealing kitchen involves a careful balance of aesthetics, ergonomics, and practicality. In this article, we will explore the essential aspects of kitchen design, guiding you through the transformative process of crafting a stunning and efficient culinary space. 
       A well-designed kitchen is more than just a place to cook; it is a space that inspires creativity, fosters efficient workflows, and accommodates the diverse needs of its users. Whether you're starting from scratch or renovating an existing kitchen, understanding your requirements, preferences, and lifestyle is crucial in creating a design that seamlessly integrates with your daily routines.
       </p>
@@ -55,8 +68,7 @@ const initialState: BlogPageState = {
       firstPostImage: firstLivingRoomImage,
       secondPostImage: secondLivingRoomImage,
       content: `Low Cost Latest Invented Interior Designing Ideas`,
-      postContent:
-      `<p>
+      postContent: `<p>
       Creating a living room that reflects your unique style, offers exceptional comfort, and embodies functional elegance requires thoughtful planning and flawless execution. The construction process for your living room is an opportunity to transform an empty space into a welcoming haven where you can relax, entertain, and make lasting memories with loved ones. In this article, we will delve into the essential aspects of building a stunning living room, guiding you from the initial design considerations to the final finishing touches.
       </p>
       <p>
@@ -72,8 +84,7 @@ const initialState: BlogPageState = {
       firstPostImage: firstInteriorImage,
       secondPostImage: secondInteriorImage,
       content: 'Best For Any Office & Business Interior Solution',
-      postContent:
-      `<p>
+      postContent: `<p>
       Interior design is the art of creating a harmonious and visually appealing space that reflects your personality, enhances functionality, and promotes a sense of well-being. Whether you are starting from scratch or looking to revamp your existing space, the process of interior design encompasses a wide range of considerations and decisions. In this article, we will explore the fundamental aspects of interior design, guiding you through the transformative journey of crafting a stunning and cohesive living space. 
       </p>
       <p>
@@ -89,8 +100,7 @@ const initialState: BlogPageState = {
       firstPostImage: firstKitchanImage,
       secondPostImage: secondKitchanImage,
       content: 'Let’s Get Solution For Building Construction Work',
-      postContent: 
-      `<p>
+      postContent: `<p>
       The kitchen is often referred to as the heart of the home, where culinary delights are created, memories are made, and families gather. Designing a functional and visually appealing kitchen involves a careful balance of aesthetics, ergonomics, and practicality. In this article, we will explore the essential aspects of kitchen design, guiding you through the transformative process of crafting a stunning and efficient culinary space. 
       A well-designed kitchen is more than just a place to cook; it is a space that inspires creativity, fosters efficient workflows, and accommodates the diverse needs of its users. Whether you're starting from scratch or renovating an existing kitchen, understanding your requirements, preferences, and lifestyle is crucial in creating a design that seamlessly integrates with your daily routines.
       </p>
@@ -107,8 +117,7 @@ const initialState: BlogPageState = {
       firstPostImage: firstLivingRoomImage,
       secondPostImage: secondLivingRoomImage,
       content: `Low Cost Latest Invented Interior Designing Ideas`,
-      postContent: 
-      `<p>
+      postContent: `<p>
       Creating a living room that reflects your unique style, offers exceptional comfort, and embodies functional elegance requires thoughtful planning and flawless execution. The construction process for your living room is an opportunity to transform an empty space into a welcoming haven where you can relax, entertain, and make lasting memories with loved ones. In this article, we will delve into the essential aspects of building a stunning living room, guiding you from the initial design considerations to the final finishing touches.
       </p>
       <p>
@@ -124,13 +133,31 @@ const initialState: BlogPageState = {
       firstPostImage: firstInteriorImage,
       secondPostImage: secondInteriorImage,
       content: 'Best For Any Office & Business Interior Solution',
-      postContent: 
-      `<p>
+      postContent: `<p>
       Interior design is the art of creating a harmonious and visually appealing space that reflects your personality, enhances functionality, and promotes a sense of well-being. Whether you are starting from scratch or looking to revamp your existing space, the process of interior design encompasses a wide range of considerations and decisions. In this article, we will explore the fundamental aspects of interior design, guiding you through the transformative journey of crafting a stunning and cohesive living space. 
       </p>
       <p>
       The interior design of your home is a reflection of your unique style and preferences. It goes beyond mere aesthetics, encompassing the thoughtful arrangement of furniture, the selection of color schemes, the play of textures, and the integration of lighting and accessories. A well-designed interior has the power to create a sanctuary that nurtures and inspires you, providing a backdrop for your daily activities and personal experiences.When embarking on an interior design project, it is crucial to start with a clear vision. Consider the overall atmosphere you wish to create and the purpose of the space. Think about how you want to feel when you enter the room and what activities will take place there. Understanding the function and mood you desire will serve as a guiding principle throughout the design process.
       </p>`,
+    },
+  ],
+
+  posts: [
+    {
+      postId: 2,
+      date: '22 December,2022',
+      tag: 'Living Design',
+      imageSrc: firstPostPicture,
+      firstPostImage: firstLivingRoomImage,
+      secondPostImage: secondLivingRoomImage,
+      content: `Low Cost Latest Invented Interior Designing Ideas`,
+      postContent: `<p>
+      Creating a living room that reflects your unique style, offers exceptional comfort, and embodies functional elegance requires thoughtful planning and flawless execution. The construction process for your living room is an opportunity to transform an empty space into a welcoming haven where you can relax, entertain, and make lasting memories with loved ones. In this article, we will delve into the essential aspects of building a stunning living room, guiding you from the initial design considerations to the final finishing touches.
+      </p>
+      <p>
+      Your living room serves as the centerpiece of your home, a space where you can showcase your personal taste and create an inviting atmosphere for gatherings and relaxation. From the layout and structural modifications to the choice of materials and finishing details, every decision you make contributes to the overall ambiance and functionality of the room. By approaching the construction process with careful attention to detail and a clear vision of your desired outcome, you can bring your dream living room to life.
+      </p>`,
+      text: 'Explore the innovative and affordable interior design concepts that are reshaping the industry, offering a fresh and modern approach to decorating your space. Discover how to achieve a stylish and up-to-date look without breaking the bank with these latest low-cost design ideas.',
     },
   ],
 };
